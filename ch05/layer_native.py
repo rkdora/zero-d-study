64634151ddd2c27bfcdfb17984e4b7527c182732
ryuto <rryuto5210@gmail.com>
2019-10-30 14:31:39 +0900

    Add AddLayer

--- ch05/layer_native.py
+++ ch05/layer_native.py
@@ -19,3 +19,17 @@ class MulLayer:
         dy = dout * self.x
 
         return dx, dy
+
+# 加算レイヤ
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
