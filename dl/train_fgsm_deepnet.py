5f150c53db6be8bee9d4f182a512223050a35ebd
ryuto <rryuto5210@gmail.com>
2019-11-25 10:02:51 +0900

    Add fgsm train

---
+++ dl/train_fgsm_deepnet.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from deep_convnet import DeepConvNet
+from common.trainer import Trainer
+from keras.datasets import mnist
+
+# データの読み込み
+(x_train, t_train), (x_test, t_test) = mnist.load_data()
+
+# 3次元から4次元へ
+x_train_shape = x_train.shape
+x_train = x_train.reshape(x_train_shape[0], 1, x_train_shape[1], x_train_shape[2])
+
+x_test_shape = x_test.shape
+x_test = x_test.reshape(x_test_shape[0], 1, x_test_shape[1], x_test_shape[2])
+
+# 正規化
+x_train = x_train.astype(np.float32)
+x_train /= 255.0
+
+x_test = x_test.astype(np.float32)
+x_test /= 255.0
+
+max_epochs = 3
+
+network = DeepConvNet()
+
+network.load_params("deep_convnet_params.pkl")
+print("loaded Network Parameters!")
+
+def generate_adv(x, label, network, eps=0.01):
+    d, g = network.gradient_for_fgsm(x.reshape(1, 1, 28, 28), np.array([label]))
+    d = d.reshape(28, 28)
+    p = eps * np.sign(d)
+    adv = (x + p).clip(min=0, max=1)
+    adv = adv.reshape(1, 28, 28)
+    return adv
+
+f_train = []
+
+for x, t in zip(x_train, t_train):
+    f_train.append(generate_adv(x, t, network, 0.3))
+
+f_train = np.array(f_train)
+
+# print(f_train.shape)
+
+trainer = Trainer(network, f_train, t_train, x_test, t_test,
+                  epochs=max_epochs, mini_batch_size=100,
+                  optimizer='Adam', optimizer_param={'lr':0.001},
+                  evaluate_sample_num_per_epoch=1000)
+trainer.train()
+
+# パラメータの保存
+network.save_params("deep_convnet_params_fgsm.pkl")
+print("Saved Network Parameters!")
+
+# グラフの描画
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(max_epochs)
+plt.plot(x, trainer.train_acc_list, marker='o', label='train', markevery=2)
+plt.plot(x, trainer.test_acc_list, marker='s', label='test', markevery=2)
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+# plt.show()
+plt.savefig('graph_fgsm.png')
